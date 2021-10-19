1337f542e1af0d76c5c4b968d33d248954a1ff10
awei.tian <awei.tian@qq.com>
2021-10-20 06:59:56 +0800

    ssh2

---
+++ 11-ssh2/src/ssh2/es.js
@@ -0,0 +1,21 @@
+var Client = require('electron-ssh2').Client;
+
+var conn = new Client();
+conn.on('ready', function() {
+  console.log('Client :: ready');
+  conn.shell(function(err, stream) {
+    if (err) throw err;
+    stream.on('close', function() {
+      console.log('Stream :: close');
+      conn.end();
+    }).on('data', function(data) {
+      console.log('OUTPUT: ' + data);
+    });
+    stream.end('cd / \nls -al\nexit\n');
+  });
+}).connect({
+  host: '127.0.0.1',
+  port: 2222,
+  username: 'gondar',
+  password: '000000'
+});
